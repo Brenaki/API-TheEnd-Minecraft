42e76acef1f4b7d1a3574ea521d399a0a379744a
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2023-05-21 23:47:04 -0300

    Update Finally
    Add English API

--- APIEnd.py
+++ APIEnd.py
@@ -44,8 +44,8 @@ def pt():
          },
          'recursos_de_terreno': {
              # Recursos de Terreno
-             'ilha_principal': 'Na ilha principal, há dez torres de obsidiana com cristais do End no topo, que regeneram a saúde do dragão. O jogador deve destruir os cristais para enfraquecer o dragão e poder atacá-lo. Quando o dragão é derrotado, ele explode e deixa cair muita experiência e um ovo de dragão. O portal de saída é ativado e permite ao jogador voltar ao mundo normal ou explorar as ilhas externas do End, onde há cidades do End e navios do End com tesouros e elytras. O dragão pode ser renascido quantas vezes o jogador quiser, colocando quatro cristais do End ao redor do portal de saída.',
-             'ilhas_externas': 'As ilhas externas do End são lugares misteriosos e isolados que só podem ser acessados após derrotar o dragão do End. Elas são compostas por ilhas flutuantes de variados tamanhos e formas, cobertas por plantas do coro que produzem frutas do coro. Nessas ilhas, o jogador pode encontrar cidades do End, que são estruturas raras e complexas que abrigam shulkers e baús com tesouros. Algumas cidades do End têm navios do End ancorados perto delas, que contêm itens ainda mais valiosos, como elytras e cabeça de dragão.'
+             'Ilha_principal': 'Na ilha principal, há dez torres de obsidiana com cristais do End no topo, que regeneram a saúde do dragão. O jogador deve destruir os cristais para enfraquecer o dragão e poder atacá-lo. Quando o dragão é derrotado, ele explode e deixa cair muita experiência e um ovo de dragão. O portal de saída é ativado e permite ao jogador voltar ao mundo normal ou explorar as ilhas externas do End, onde há cidades do End e navios do End com tesouros e elytras. O dragão pode ser renascido quantas vezes o jogador quiser, colocando quatro cristais do End ao redor do portal de saída.',
+             'Ilhas_externas': 'As ilhas externas do End são lugares misteriosos e isolados que só podem ser acessados após derrotar o dragão do End. Elas são compostas por ilhas flutuantes de variados tamanhos e formas, cobertas por plantas do coro que produzem frutas do coro. Nessas ilhas, o jogador pode encontrar cidades do End, que são estruturas raras e complexas que abrigam shulkers e baús com tesouros. Algumas cidades do End têm navios do End ancorados perto delas, que contêm itens ainda mais valiosos, como elytras e cabeça de dragão.'
          },
          'mobs': {
              # Mobs
@@ -58,14 +58,14 @@ def pt():
 
                     # Informações
 
-                    ## Comportamento
+                    ## Compotamento
                     'comportamento': 'Neutro',
                     'comportamento_pacifico': 'Neutro',
 
                     ## Geração
                     'geracao': 'Em todo Fim, com níveis de iluminação 0',
 
-                    ## Dano por Vida
+                    ## Dano por vida
                     'dano_por_vida_facil': 4.5,
                     'dano_por_vida_normal': 7,
                     'dano_por_vida_dificil': 10.5,
@@ -127,8 +127,8 @@ def pt():
                     # Informações
 
                     ## Comportamento
-                    'comportamento': None,
-                    'comportamento_pacifico': None,
+                    'comportamento': 'Hostil',
+                    'comportamento_pacifico': 'Hostil',
 
                     ## Geração
                     'geracao': 'O Fim',
@@ -303,15 +303,294 @@ def pt():
 def en():
     return {
         # Accessing
-        'access': {'A': 'B'},
+        'Access': 'To reach the End, the player needs to find a stronghold using eyes of ender and locate the portal room. The room has an End portal with 12 frames, some with eyes already inserted. The player needs to complete the portal with any remaining ender eyes. When activating the portal, a loud sound is emitted and the portal replaces the center blocks with End portal blocks. The player can enter the portal to go to the End, along with other entities that were teleported before.',
         # Environment
-        'environment': {'B': 'Biomes', 'G': 'Generated Structured','T': 'Terrain Features', 'M': 'Mobs', 'Bl': 'Blocks'},
+        'Environment': {
+                        'biomes': {
+                            # Biomes
+                            '1': 'The End',
+                            '2': 'Small End islands',
+                            '3': 'End midlands',
+                            '4': 'End highlands',
+                            '5': 'End barrens',
+                        },
+                        'generated_structured': {
+                            # Generated Structures
+                            '1': 'Central island',
+                            '2': 'Outer islands',
+                            '3': 'End spike',
+                            '4': 'Obsidian plataform',
+                            '5': 'Exit portal',
+                            '6': 'End gateway',
+                            '7': 'End city',
+                            '8': 'End ship',
+                            '9': 'Chorus tree'
+                        },
+                        'terrain_features': {
+                            # Terrain Features
+                            'Main_island': 'On the main island, there are ten obsidian towers with Ender crystals on top, which regenerate the dragon\'s health. The player must destroy the crystals to weaken the dragon and be able to attack it. When the dragon is defeated, it explodes and drops a lot of experience and a dragon egg. The exit portal is activated and allows the player to return to the normal world or explore the outer islands of the End, where there are cities of the End and ships of the End with treasure and elytra. The dragon can be respawned as many times as the player wants by placing four Ender crystals around the exit portal.',
+                            'Outer_island': 'The outer islands of the End are mysterious and isolated places that can only be accessed after defeating the dragon of the End. They are made up of floating islands of varying sizes and shapes, covered with chorus plants that produce chorus fruits. On these islands, the player can find End Cities, which are rare and complex structures that house shulkers and treasure chests. Some End cities have End ships docked near them, which contain even more valuable items, such as elytra and dragon\'s head.'
+                        },
+                        'mobs': {
+                            # Mobs
+                            '1':{
+                                # Enderman
+                                'Enderman': {
+                                # Life
+                                'life': 40,
+                                'hearts': 20,
+
+                                # Information
+
+                                ## Behavior
+                                'behavior': 'Neutral',
+                                'behavior_peaceful': 'Neutral',
+
+                                ## Spawn
+                                'spawn': 'The End with 0 level light',
+
+                                ## Damage for life
+                                'damage_for_life_easy': 4.5,
+                                'damage_for_life_normal': 7,
+                                'damage_for_life_hard': 10.5,
+
+                                ## Damage for coração
+                                'damage_for_hearts_easy': 2.25,
+                                'damage_for_hearts_normal': 3.5,
+                                'damage_for_hearts_hard': 5.25,
+
+                                ## Effect after a damage and effect duration
+                                'damage_effect': None,
+                                'duration_damage_effect': None,
+
+                                # Drops
+                                'drop1': 'Ender pearl',
+                                'drop2': '5 XP',
+                                'drop3': 'Any block being held'
+                                }
+                            },
+                            '2':{
+                                # Shulker
+                                'Shulker': {
+                                    # Life
+                                    'life': 30,
+                                    'hearts': 15,
+
+                                    # Information
+
+                                    ## Behavior
+                                    'behavior': 'Hostile',
+                                    'behavior_peaceful': 'Passive',
+
+                                    ## Spawn
+                                    'spawn': 'End city',
+
+                                    ## Damage for life
+                                    'damage_for_life': 4,
+
+                                    ## Damage for hearts
+                                    'damage_for_hearts': 2,
+
+                                    ## Effect after a damage and effect duration
+                                    'damage_effect': 'Levitation',
+                                    'duration_damage_effect': '10 seconds',
+
+                                    # Drops
+                                    'drop1': 'Shulker shell',
+                                    'drop2': '5 XP'     
+                                }
+                            },
+                            '3':{
+                                # Ender Dragon
+                                'Ender_Dragon': {
+                                    # Life
+                                    'life': 200,
+                                    'hearts': 100,
+
+                                    # Information
+
+                                    ## Behavior
+                                    'behavior': 'Hostile',
+                                    'behavior_peaceful': 'Hostile',
+
+                                    ## Spawn
+                                    'spawn': 'The End',
+
+                                    ## damage for life
+                                    'damage_for_life_peaceful': 0,
+                                    'damage_for_life_easy': 6,
+                                    'damage_for_life_normal': 10,
+                                    'damage_for_life_hard': 15,
+
+                                    ## damage for hearts
+                                    'damage_for_hearts_peaceful': 0,
+                                    'damage_for_hearts_easy': 3,
+                                    'damage_for_hearts_normal': 5,
+                                    'damage_for_hearts_hard': 7.5,
+
+                                    ## Special Attack
+                                    'special_attack': {
+                                    '1': {
+                                        'name': 'Dragon\'s Breath',
+                                        'damage_life': 3,
+                                        'damage_hearts': 1.5, 
+                                        'duration': '0,5 segundos'
+                                        },
+                                    '2': {
+                                        'name': 'Dragon Fireball',
+                                        'damage_life': 6,
+                                        'damage_hearts': 3, 
+                                        'duration': '0,5 segundos'
+                                        }
+                                    },
+
+                                    ## Effect after a damage and effect duration
+                                    'damage_effect': None,
+                                    'duration_damage_effect': None,
+
+                                    # Drops
+                                    'drop1': '12000 XP',
+                                    'drop2': 'Dragon egg'
+
+                                }
+                            }
+                        },
+                        'blocks': {
+                            # Blocks
+                            '1':{
+                                # Naturally generated
+                                '1': 'Air',
+                                '2': 'Bedrock',
+                                '3': 'Obsidian',
+                                '4': 'End stone',
+                                '5': 'Chorus plant',
+                                '6': 'Chorus flower'
+                            },
+                            '2':{
+                                # Naturally created
+                                '1': 'Air',
+                                '2': 'Bedrock',
+                                '3': 'Obsidian',
+                                '4': 'Wall torch',
+                                '5': 'Fire',
+                                '6': 'End portal',
+                                '7': 'Dragon egg',
+                                '8': 'End gateway',
+                                '9': 'End crystal',
+                                '10': 'Iron bars',
+                                '11': 'End stone'
+                            },
+                            '3':{
+                                # Structures
+                                '1': 'Air',
+                                '2': 'Obsidian',
+                                '3': 'Chest',
+                                '4': 'Magenta stained Glass',
+                                '5': 'Brewing stand',
+                                '6': 'Item frame',
+                                '7': 'Magenta wall banner',
+                                '8': 'End rod',
+                                '9': 'Purpur block',
+                                '10': 'Purpur pillar',
+                                '11': 'Purpur slab',
+                                '12': 'Purpur stairs',
+                                '13': 'End stone bricks',
+                                '14': 'Dragon wall head',
+                                '15': 'Ender chest',
+                                '16': 'Ladder',
+                            }
+                        } 
+                        },
         # Technical Information
-        'technical_information': {'C': 'D'},
+        'Technical_information': {
+            # ID
+            'ID': {
+                'Name': 'The End',
+                'ID_JE': 'the_end',
+                'Numeric_ID': 1
+                }
+        },
         # Achievements
-        'achievements': {'TE': 'The End?', 'te': 'The End.', 'YM': 'You Need a Mint', 'TA': 'The End... Again...', 'GH': 'Great View From Up Here'},
+        'Achievements': {
+            '1': {
+                'Name': 'The End?',
+                'Description': 'Enter an End Portal'
+            },
+            '2': {
+                'Name': 'The End',
+                'Description': 'Kill the Enderdragon'
+            },
+            '3': {
+                'Name': 'You Need a Mint',
+                'Description': 'Collect dragons breath in a glass bottle'
+            },
+            '4': {
+                'Name': 'The End... Again...',
+                'Description': 'Respawn the Enderdragon'
+            },
+            '5': {
+                'Name': 'Great View From Up Here',
+                'Description': 'Levitate up 50 blocks from the attacks of a Shulker'
+            }
+        }, 
         # Advancements
-        'advancements': {'TE': 'The End?', 'FE': 'Free The End', 'TG':'The Next Generation', 'RG': 'Remote Getaway', 'TA': 'The End... Again...', 'YM': 'You Need a Mint', 'TG': 'The City at the End of the Game', 'SL': 'Sky\'s the Limit', 'GH': 'Great View From Up Here'}
+        'Advancements': {
+            '1': {
+                'Name': 'The End?',
+                'Description': 'Or the beginning?',
+                'Requirements': 'Enter the End dimension.',
+                'ID': 'minecraft:end/root'
+            },
+            '2': {
+                'Name': 'Free the End',
+                'Description': 'Good luck',
+                'Requirements': 'Kill the ender dragon. If multiple players are involved in the dragon fight, only the player that deals the final blow to the dragon receives the advancement.',
+                'ID': 'minecraft:end/kill_dragon'
+            },
+            '3': {
+                'Name': 'The Next Generation',
+                'Description': 'Hold the Dragon Egg',
+                'Requirements': 'Have a dragon egg in your inventory.',
+                'ID': 'minecraft:end/dragon_egg'
+            },
+            '4': {
+                'Name': 'Remote Getaway',
+                'Description': 'Escape the island',
+                'Requirements': 'Throw an ender pearl through, fly, or walk into an end gateway.',
+                'ID': 'minecraft:end/enter_end_gateway'
+            },
+            '5': {
+                'Name': 'The End... Again...',
+                'Description': 'Respawn the Ender Dragon',
+                'Requirements': 'Be in a certain radius from the exit portal when an ender dragon is summoned using end crystals.',
+                'ID': 'minecraft:end/respawn_dragon'
+            },
+            '6': {
+                'Name': 'You Need a Mint',
+                'Description': 'Collect dragon\'s breath in a glass bottle',
+                'Requirements': 'Have a bottle of dragon\'s breath in your inventory.',
+                'ID': 'minecarft:end/dragon_breath'
+            },
+            '7': {
+                'Name': 'The City at the End of the Game',
+                'Description': 'Go on in, what could happen?',
+                'Requirements': 'Enter an end city.',
+                'ID': 'minecraft:end/find_end_city'
+            },
+            '8': {
+                'Name': 'Sky\'s the Limit',
+                'Description': 'Find elytra',
+                'Requirements': 'Have a pair of elytra in your inventory.',
+                'ID': 'minecraft:end/elytra'
+            },
+            '9': {
+                'Name': 'Great View From Up Here',
+                'Description': 'Levitate up 50 blocks from the attacks of a Shulker',
+                'Requirements': 'Move a distance of 50 blocks vertically with the Levitation effect applied, regardless of direction or whether it is caused by the effect.',
+                'ID': 'minecraft:end/levitate'
+            }
+        }
+        
     }
 
 # Start a API - Inicia a API
